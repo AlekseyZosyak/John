426200b2d628c97e8b8a82596051c24ab5f7ddf7
AlekseyZosyak <110170863+AlekseyZosyak@users.noreply.github.com>
2023-01-12 03:12:22 +0200

    Delete JS directory

--- JS/resume.js
+++
@@ -1,8 +0,0 @@
-let bottonResume = document.querySelector('botton[data-open-resume]');
-bottonResume.addEventListener('click', modal);
-let bottonCloset = document.querySelector('botton[data-closet-resume]');
-bottonCloset.addEventListener('click', modal);
-let backgraund = document.querySelector('div[data-backgraund');
-function modal() {
-    backgraund.classList.toggle('is-hidden');
-}
